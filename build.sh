58a2a9e59bca5a3845af8a26ffb8d31393944e00
Kovalit31 <nonecone20@gmail.com>
2023-05-25 16:37:43 +0300

    Dev: Writing our builder

--- build.sh
+++ build.sh
@@ -7,4 +7,49 @@ ARCH=$(uname -m | sed -e s/i.86/x86/ -e s/x86_64/x86_64/ \
 				  -e s/ppc.*/powerpc/ -e s/mips.*/mips/ \
 				  -e s/sh[234].*/sh/ -e s/aarch64.*/arm64/)
 
-echo $ARCH
+CURPWD=$(pwd)
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+# Check availability of commands
+WGET=true
+command -v wget > /dev/null 2>&1 || {
+    WGET=false
+}
+
+INSTALL=false
+command -v rustup >/dev/null 2>&1 || {
+    if $WGET; then
+        echo "I will install rustup after 5 seconds..."
+        INSTALL=true
+    else
+        echo "No rustup and wget found. Abort!"; exit 1
+    fi
+}
+
+if $INSTALL; then
+    [ wget -O- http://sh.rustup.rs | sh - -y --profile minimal ] || echo "Error occured. Abort."; exit 1
+fi
+
+# Set toolchains default profile
+rustup set profile default
+
+# Adding components
+install_components() {
+    rustup component add rust-std llvm-tools-preview cargo
+    cargo install cargo-xbuild bootimage
+}
+
+if [ $ARCH = "x86_64" ]; then
+    rustup toolchain install nightly-x86_64-unknown-linux-gnu
+    rustup default nightly-x86_64-unknown-linux-gnu
+    install_components
+    cd $DIR
+    cargo xbuild --target configs/x86_64-kernel.json
+    cargo bootimage    
+fi
