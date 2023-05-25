c817adf0536425f1689e47df9e2d2c72fb5217e3
Kovalit31 <nonecone20@gmail.com>
2023-05-25 17:11:28 +0300

    Dev: ...

--- build.rs
+++ build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("cargo:rustc-link-arg=-nostartfiles,-Wl,--allow-multiple-definition")
+    println!("cargo:rustc-link-arg=-Wl,--allow-multiple-definition,-Wl,--nostartfiles")
 }
